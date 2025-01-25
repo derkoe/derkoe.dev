1e435a6d0ed548b45b9ed8b03a4b033b544b56d2
Christian Köberl <christian.koeberl@gmail.com>
2025-01-25 15:16:30 +0100

    add Netlify, run Prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
       theme: "github-dark",
-      wrap: true
-    }
-  }
+      wrap: true,
+    },
+  },
+
+  adapter: netlify(),
 });
